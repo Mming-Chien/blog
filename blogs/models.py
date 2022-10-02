844bedfcbd55562a2d31267d88cf239df23395dc
Mming-Chien <113403394+Mming-Chien@users.noreply.github.com>
2022-10-02 11:49:22 +0700

    Home page
    Home page display for display blogs title, text, date added

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BlogPost(models.Model):
+	"""The blog the user want to add"""
+	title= models.CharField(max_length = 300)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add= True)
+
+	def __str__(self):
+		""" Return the string representation of the model"""
+		return self.title
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import BlogPost
 # Create your views here.
+
+def index(request):
+	""" The home page for blogs """
+	blogs = BlogPost.objects.order_by("date_added")
+	context = {"blogs": blogs}
+	return render(request, "blogs/index.html", context)
