a43ba0a39e2efaf339c8e723476c44c0488c6cce
Mming-Chien <113403394+Mming-Chien@users.noreply.github.com>
2022-10-03 09:18:20 +0700

    Revert "pages for add and edit blog"
    This reverts commit 73035542605bc1d6bab636f13fcec0e55574c8fd.

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,9 +6,5 @@ app_name = "blogs"
 urlpatterns = [
 	#Home page
 	path("", views.index , name = "index"),
-	# Page for adding new blogs
-	path("new_blog", views.new_blog, name="new_blog"),
-	# Page for editing blog
-	path("edit_blog/<int:blog_id>/", views.edit_blog, name= "edit_blog"),
 
 ]
